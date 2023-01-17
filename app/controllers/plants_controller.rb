ad1af4214a9c101864480c25a4971cc3e38c24a7
amyhskwon <amyhskwon@gmail.com>
2023-01-17 02:03:36 -0800

    commit tree b97c6b7533e59491d6c41cd4528d4da33b10e5cc

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = Plant.find(params[:id])
     render json: plant
   end
 
@@ -18,9 +18,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find(params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      plant_not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    if plant
+      plant.destroy
+      head:no_content
+    else
+      plant_not_found
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def plant_not_found
+    render json: {error: "Plant not found"}, status: :not_found
   end
 end
